9d52c0833c93a3cf8748545f27f4c81fdcb3f8c4
shiphan-pathan <shiphanpathan2001@gmail.com>
2024-09-01 11:57:39 +0530

    Done

---
+++ Backend/demo05/src/main/java/com/example/demo05/Demo05Application.java
@@ -0,0 +1,17 @@
+package com.example.demo05;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class Demo05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo05Application.class, args);
+	}
+
+}
